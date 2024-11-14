ff2db2b62b4357703b4739ecf39e202f84a39835
Patryk Mysliwiec <EMAIL>
2024-11-14 11:26:02 +0100

    asdasd

--- znajdz.php
+++ znajdz.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kwaity</title>
+    <title>Kwiaty</title>
     <link rel="stylesheet" href="styl3.css">
 </head>
 <body>
@@ -15,7 +15,7 @@
         <ol>
             <li><a href="./index.html">Strona główna</a></li>
             <li><a href="https://www.kwiaty.pl/" target="_blank">Rozpoznaj kwiaty</a></li>
-            <li><a href="znajdz.php"></a>
+            <li><a href="znajdz.php">Znajdź Kwiaciarnie</a></li>
                 <ul>
                     <li>w Warszawie</li>
                     <li>w Malborku</li>
@@ -25,7 +25,24 @@
         </ol>
     </aside>
     <main>
-        
+        <h2>Znajdź kwiaciarnię</h2>
+        <form action="./znajdz.php" method="POST">
+            Podaj nazwę miasta:<input type="text" name="znajdz" id="">
+            <input type="submit" name="" id="" value="SPRAWDŹ">
+        </form>
+        <?php
+        $polaczenie = mysqli_connect('localhost', 'root', '', 'kwiaciarnia');
+        if (isset($_POST['znajdz'])) {
+            $znajdz = $_POST['znajdz'];
+            $sql = "SELECT `nazwa`, `ulica` FROM kwiaciarnie WHERE miasto = '$znajdz';";
+            $wynik = mysqli_query($polaczenie, $sql);
+            while ($wiersz = mysqli_fetch_assoc($wynik)) {
+                echo "<h3>".$wiersz['nazwa'].", ".$wiersz['ulica']."</h3>";
+            }
+            mysqli_close($polaczenie);
+        }
+
+        ?>
     </main>
     <footer>
         <p>Stronę opracował: 1234</p>
